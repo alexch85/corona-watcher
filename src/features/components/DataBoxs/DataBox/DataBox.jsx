174275ca3b507296b727d7ec49870bebb680d6c7
Alex <alexch85@windowslive.com>
2020-10-07 10:58:49 +0300

    Logo svg file changed DataBox component updated

--- src/features/components/DataBoxs/DataBox/DataBox.jsx
+++ src/features/components/DataBoxs/DataBox/DataBox.jsx
@@ -8,11 +8,21 @@ export default function DataBox({ ctgColor, category, number }) {
 			<Typography variant='h3' color='secondary'>
 				390,229
 			</Typography>
-			<div style={{ width: '200px', height: '35px', margin: '10px 0', backgroundColor: ctgColor }}>
+			<div className={styles.categoryBox}>
+				<div
+					style={{
+						width: '30px',
+						height: '20px',
+						margin: '10px 0',
+						backgroundColor: ctgColor,
+						marginRight: '5px',
+					}}
+				/>
 				<Typography variant='h5' color='secondary'>
 					{category}
 				</Typography>
 			</div>
+
 			<Typography color='secondary'>Total recovered form covid-19 by {new Date().toDateString()}</Typography>
 		</div>
 	);
