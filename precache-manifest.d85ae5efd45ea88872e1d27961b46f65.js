9ff4275050a8ef1a88a03c44c4e02488584cb4b9
Alex <alexch85@windowslive.com>
2020-10-09 13:19:33 +0300

    Updates

---
+++ precache-manifest.d85ae5efd45ea88872e1d27961b46f65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ee24d5f9a24d8510edda0fa69129b0",
+    "url": "/corona-watcher/index.html"
+  },
+  {
+    "revision": "b501f2fc3a5ea6e2e3f4",
+    "url": "/corona-watcher/static/css/main.ee9b0d38.chunk.css"
+  },
+  {
+    "revision": "1d619762897cacd4d31d",
+    "url": "/corona-watcher/static/js/2.443a22d7.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/corona-watcher/static/js/2.443a22d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b501f2fc3a5ea6e2e3f4",
+    "url": "/corona-watcher/static/js/main.dc0c4b73.chunk.js"
+  },
+  {
+    "revision": "434765be6ca7d4144e3c",
+    "url": "/corona-watcher/static/js/runtime-main.b482ac6d.js"
+  }
+]);
