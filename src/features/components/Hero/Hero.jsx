a586f798a6ea14b1dcc6e54cb0a58f828e28e707
Alex <alexch85@windowslive.com>
2020-10-09 14:08:11 +0300

    deployed to git hub pages again

--- src/features/components/Hero/Hero.jsx
+++ src/features/components/Hero/Hero.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './Hero.module.css';
 
-export default function Hero({ darkMode, handleDarkMode }) {
+export default function Hero() {
 	return (
 		<div className={styles.heroContainer}>
-			<img alt='logo' src='/images/cw-logo.svg' />
+			<img alt='logo' src='https://i.postimg.cc/4x348tg7/cw-logo-0-5x.png' />
 		</div>
 	);
 }
